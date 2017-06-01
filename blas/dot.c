8d57503c900f02dc082931fc7227090769bb25fb
Joao Carlos Menezes Carreira <joao@berkeley.edu>
2017-06-01 16:28:57 -0700

    Refactoring

--- blas/dot.c
+++ blas/dot.c
@@ -17,12 +17,11 @@ float dot(float* a, float* b, uint64_t size) {
 int main() {
 
     float* a, *b;
-    a = malloc(sizeof(float) * (1<<LIMIT));
-    b = malloc(sizeof(float) * (1<<LIMIT));
+    a = malloc(sizeof(float) * (1 << LIMIT));
+    b = malloc(sizeof(float) * (1 << LIMIT));
 
     for (uint64_t i = 1; i < LIMIT; ++i) {
         uint64_t count = (1 << i);
-
 
         float res = 0;
         for (uint64_t j = 1; j < count;++j) {
@@ -36,16 +35,18 @@ int main() {
         double r = cblas_sdot(count, a, 1, b, 1);
         gettimeofday(&tv_end, NULL);
 
-        uint64_t mics_blas = (tv_end.tv_sec * MIC_IN_SEC + tv_end.tv_usec) - (tv.tv_sec * MIC_IN_SEC + tv.tv_usec);
+        uint64_t mics_blas = (tv_end.tv_sec * MIC_IN_SEC + tv_end.tv_usec) -
+                             (tv.tv_sec * MIC_IN_SEC + tv.tv_usec);
         
         struct timeval tv2, tv_end2;
         gettimeofday(&tv2, NULL);
         double r2 = dot(a, b, count);
         gettimeofday(&tv_end2, NULL);
-        uint64_t mics_hand = (tv_end2.tv_sec * MIC_IN_SEC + tv_end2.tv_usec) - (tv2.tv_sec * MIC_IN_SEC + tv2.tv_usec);
+        uint64_t mics_hand = (tv_end2.tv_sec * MIC_IN_SEC + tv_end2.tv_usec) -
+                             (tv2.tv_sec * MIC_IN_SEC + tv2.tv_usec);
 
-        printf("size: %lu spent %lu\n", count, mics_blas);
-        printf("size: %lu spent %lu\n", count, mics_hand);
+        printf("BLAS\tsize: %lu\tspent %lu\n", count, mics_blas);
+        printf("MANUAL\tsize: %lu\tspent %lu\n", count, mics_hand);
 
         printf("%lf %lf %lf\n", res, r, r2);
     }
